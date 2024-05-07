f938ca9af8b8640d4f0aad7179589de51e89d439
alexfargo <kevin.a.fargo@gmail.com>
2024-05-06 20:21:05 -0600

    Add authentication tests for user registration and login

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -9,8 +9,24 @@ chai.use(chaiHttp);
 //the service must be running for these test to run!
 //the version of mocha and chai matter here becasue newest versions require some additional refactoring to get to work.
 
+before((done) => {
+  chai
+    .request("http://localhost:3000")
+    .delete("/api/auth/delete")
+    .send({
+      userEmail: "test@mocha.com",
+      userName: "mochatest",
+      userPassword: "test",
+    })
+    .end((_, resp) => {
+      console.log(
+        "If the test user already existed, it's getting deleted now!" + "\n"
+      );
+      done();
+    });
+});
+
 describe("Auth API service", () => {
-  
   step("should POST a new user", (done) => {
     chai
       .request("http://localhost:3000")
@@ -21,7 +37,10 @@ describe("Auth API service", () => {
         userEmail: "test@mocha.com",
       })
       .end((_, resp) => {
-        console.log(resp.text);
+        //color codes the text in the console
+        console.log("\n" + "\x1b[35m" + "Registering a new user" + "\x1b[0m");
+        console.log("Response Status: " + "\x1b[36m%s\x1b[0m", resp.status);
+        console.log(resp.body);
         expect(resp.body.userEmail).to.be.equal("test@mocha.com");
         done();
       });
@@ -37,8 +56,51 @@ describe("Auth API service", () => {
         userEmail: "test@mocha.com",
       })
       .end((_, resp) => {
+        console.log(
+          "\n" + "\x1b[35m" + "Trying to register an existing user" + "\x1b[0m"
+        );
+        console.log("Response Status: " + "\x1b[36m%s\x1b[0m", resp.status);
         console.log(resp.text);
         expect(resp.text).to.be.equal("User with this email already exists");
+        done();
+      });
+  });
+
+  step("should fail to login a user with a incorrect password", (done) => {
+    chai
+      .request("http://localhost:3000")
+      .post("/api/auth/login")
+      .send({
+        userPassword: "wrongpassword",
+        userEmail: "test@mocha.com",
+      })
+      .end((_, resp) => {
+        console.log(
+          "\n" + "\x1b[35m" + "Trying to login with a bad password" + "\x1b[0m"
+        );
+        console.log("Response Status: " + "\x1b[36m%s\x1b[0m", resp.status);
+        console.log(resp.body.msg);
+        expect(resp.body.msg).to.be.equal("Invalid password!");
+        done();
+      });
+  });
+
+  step("should login a valid user", (done) => {
+    chai
+      .request("http://localhost:3000")
+      .post("/api/auth/login")
+      .send({
+        userPassword: "test",
+        userEmail: "test@mocha.com",
+      })
+      .end((_, resp) => {
+        console.log(
+          "\n" + "\x1b[35m" + "logging in with a good password" + "\x1b[0m"
+        );
+        console.log("Response Status: " + "\x1b[36m%s\x1b[0m", resp.status);
+        console.log(resp.body);
+        expect(resp.body.msg).to.be.equal("Logged in!");
+        expect(resp.status).to.be.equal(200);
         done();
       });
   });
@@ -53,10 +115,11 @@ describe("Auth API service", () => {
         userEmail: "test@mocha.com",
       })
       .end((_, resp) => {
-        console.log(resp.text);
+        console.log("\n" + "\x1b[35m" + "Deleting the user" + "\x1b[0m");
+        console.log("Response Status: " + "\x1b[36m%s\x1b[0m", resp.status);
+        console.log(resp.body);
         expect(resp.body.userEmail).to.be.equal("test@mocha.com");
         done();
       });
   });
- 
 });
